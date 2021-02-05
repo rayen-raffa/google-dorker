1ea575f6bed6b1301f6289c830a49b553f13309f
Rayen RAFFA <raffa.rayen@gmail.com>
2021-02-05 01:01:12 +0100

    fetch url using one key,id pair

--- dorker.py
+++ dorker.py
@@ -49,6 +49,8 @@ def read_credentials():
         for row in lines:
             if (not credentials['current']):
                 credentials['current'] = 0
+            # Add location of pair in credentials dictionary
+            row['id'] = credentials['total']
             # Add (id, key) pair to dictionary
             credentials[str(credentials['total'])] = row
             credentials['total'] += 1
@@ -57,27 +59,46 @@ def read_credentials():
     return credentials
 
 # Search the dork string and retrieve urls
-def get_urls(search_string,API_KEY,CSE_ID,start):
+def get_urls(credentials,search_string,start):
     temp = []
-    url = 'https://www.googleapis.com/customsearch/v1'
-    payload = {'key':API_KEY, 'cx':CSE_ID, 'q': search_string, 'start': start}
+    url = "https://www.googleapis.com/customsearch/v1?key={0}&cx={1}&q={2}&start={3}".format(credentials['0']['API_KEY'], credentials['0']['CSE_ID'], search_string, start)
+    # payload = {'key':credentials['0']['API_KEY'], 'cx':credentials['0']['CSE_ID'], 'q': search_string, 'start': start}
     # my_headers = {'User-agent': 'Mozilla/11.0'}
-    r = requests.get(url, params=payload) #, headers=my_headers
-    print(r.status_code)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    divtags = soup.find_all('div', class_='kCrYT')
-
-    for div in divtags:
+    try:
+        response = requests.get(url) #, headers=my_headers
+        print(response.status_code)
+        print(json.dumps(response.json()['queries']['request'][0]['searchTerms'], sort_keys=False, indent=4))
+    except Exception:
+        print("ERROR fetching from response starting at {} ! Skipping ...".format(start))
+    
+    if (response):
+        json_data = response.json()
         try:
-            temp.append(re.search('url\?q=(.+?)\&sa', div.a['href']).group(1))
-        except:
-            continue
+            items = json_data["items"]
+            print("Found {} urls ..".format(len(temp)))
+        except KeyError:
+            print("ALERT : No response found, skipping ...")
+            items = []
+        
+        for item in items:
+            temp.append(item["link"])
+
+    # soup = BeautifulSoup(r.text, 'html.parser')
+    # divtags = soup.find_all('div', class_='kCrYT')
+
+    # for div in divtags:
+    #     try:
+    #         temp.append(re.search('url\?q=(.+?)\&sa', div.a['href']).group(1))
+    #     except:
+    #         continue
+    
+    
     return temp
 
 # Join search terms in a single dork string
 def create_dork_string(domain, file_name):
 
-    file_path = './wordlists/{}'.format(file_name)
+    file_path = './{}'.format(file_name)
 
     if not os.path.isfile(file_path):
         print('File "' +file_path+ '" does not exist')
@@ -93,7 +114,9 @@ def create_dork_string(domain, file_name):
                     dork_string = 'inurl:' + line.strip()
                     continue
                 dork_string += ' OR inurl:' + line.strip()
-        return 'site:' + domain + ' ' + dork_string
+        dork = 'site:' + domain + ' ' + dork_string
+        print('DORK Created : \\\  {}  ///'.format(dork))
+        return dork
     except:
         print('Error occured while reading file')
         sys.exit()
@@ -112,11 +135,14 @@ def main():
     pages = arguments['<pages>']
     processes = int(arguments['<processes>'])
 
+    # Read credentials
+    credentials = read_credentials()
+
     # Create search string
     search = create_dork_string(domain, file_name)
 
     # Multi-Processing
-    make_request = partial(get_urls, search)
+    make_request = partial(get_urls, credentials, search)
     pagelist = [str(x*10) for x in range(0, int(pages))]
     with Pool(processes) as p:
         tmp = p.map(make_request, pagelist)
@@ -126,11 +152,14 @@ def main():
     # Remove duplicate urls
     result = list(set(result))
     
+    
     print(*result, sep='\n')
+    with open("./urls-{}.csv".format(timer()), 'a') as res_file:
+        res_file.writelines(result)
     print('\nTotal URLs Scraped : %s ' % str(len(result)))
     print('Script Execution Time : %s ' % (timer() - start, ))
 
 
 if __name__ == '__main__':
-    # main()
-    credentials = read_credentials()
+    main()
+    # read_credentials()
